4d8fbb9e10692d86eb324d532b1a230c8de8cf8d
shiventhedestroyer <shiventhedestroyer@gmail.com>
2024-11-10 15:59:49 +0200

    first commit all

---
+++ website/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript code to handle button click event
+document.getElementById("greetButton").addEventListener("click", function() {
+    alert("Hello! Welcome to my website!"); // Shows a pop-up alert
+    console.log("Button clicked! Greeting displayed."); // Logs a message to the console
+});
