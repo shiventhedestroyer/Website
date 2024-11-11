49ef19487a9aef73cacfc673eab440eae5302e4a
shiventhedestroyer <shiventhedestroyer@gmail.com>
2024-11-11 20:35:15 +0200

    trial run

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // JavaScript code to handle button click event
 document.getElementById("greetButton").addEventListener("click", function() {
-    alert("Hello! Welcome to my website!"); // Shows a pop-up alert
+    alert("Shiven Loves Aditi"); // Shows a pop-up alert
     console.log("Button clicked! Greeting displayed."); // Logs a message to the console
 });
